673cebdd26269bdc4be67e99fd949ef3c2de9d16
xurenlu <xurenlu@gmail.com>
2009-10-31 14:10:59 +0800

    调整一下目

--- docs/gen.sh
+++ docs/gen.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-txt2tags --toc -t html DEV.rule
+#txt2tags --toc -t html DEV.rule
+for c in txt2tags_source/*.txt
+do
+    #echo $c
+    echo txt2tags $c
+    txt2tags $c
+done
+mv txt2tags_source/*.html ./html/
+#txt2tags --toc -t html DEV.rule
